3ffb294bce2caf63486406c38589c2d365901909
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-05-10 00:50:22 +0900

    add dataset maker for stockmark ner wikipedia dataset

--- allennlp_ner_ja/data/dataset_makers/__init__.py
+++ allennlp_ner_ja/data/dataset_makers/__init__.py
@@ -2,3 +2,6 @@ from allennlp_ner_ja.data.dataset_makers.dataset_maker import (  # isort:ignore 
     DatasetMaker,
 )
 from allennlp_ner_ja.data.dataset_makers.kwdlc_maker import KwdlcDatasetMaker  # NOQA
+from allennlp_ner_ja.data.dataset_makers.stockmark_ner_wiki import (  # NOQA
+    StockmarkNerWikiDatasetMaker,
+)
